b485e427afb99d568afc114bda0d91f164579601
Gonzalo Ariel Rossi <gonzaloarielrossi@gmail.com>
2022-08-08 19:44:01 -0300

    google verify added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const { generateJWT } = require('../helpers/generateJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req = request, res = response) => {
   const { mail, pwd } = req.body;
@@ -39,6 +40,49 @@ const login = async (req = request, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, img, mail } = await googleVerify(id_token);
+
+    let user = await User.findOne({ mail });
+
+    if (!user) {
+      //create user
+      const userData = {
+        name,
+        mail,
+        pwd: 'x',
+        img,
+        google: true
+      };
+
+      user = new User(userData);
+      await user.save();
+    }
+    if (!user.state) {
+      return res.status(401).json({
+        msg: 'Usuario invalido, hable con el administrador'
+      });
+    }
+
+    //generar JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      msg: 'algo salió mal',
+      ok: false
+    });
+  }
+};
 module.exports = {
-  login
+  login,
+  googleSignIn
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -16,4 +16,10 @@ router.post(
   login
 );
 
+//GOOGLE
+router.post(
+  '/google',
+  [check('id_token', 'Toke Id es necesario').not().isEmpty(), validateFields],
+  googleSignIn
+);
 module.exports = router;
